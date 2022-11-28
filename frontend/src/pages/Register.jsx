ec125502460297c9abfd3f4dfd6f78b87894d52d
Muhaz <muhazvla313@gmail.com>
2022-11-28 19:17:12 +0530

    updated Registration page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,20 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [input, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("/auth/register", input);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>REGISTER</h1>
       <form>
-        <input required type="text" placeholder="username"/>
-        <input required type="email" placeholder="email"/>
-        <input required type="password" placeholder="password"/>
-        <button>Register</button>
-        <p>Oops...Something went wrong!</p>
-        <span>Aldready a member? <Link style={{textDecoration: "none", color : '#ff9899'}} to="/login">Login here</Link></span>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
+        <span>
+          Aldready a member?{" "}
+          <Link
+            style={{ textDecoration: "none", color: "#ff9899" }}
+            to="/login"
+          >
+            Login here
+          </Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
